cd389b2c51a10a8621b97b2b841214d4646c9b94
Kyle Arens <arenskyle@gmail.com>
2017-10-15 17:17:59 -0400

    updated stuff

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
-import 
+import massage
 
 def main():
+    datafileA = open('../data/twitter-2016dev-A.txt')
+    taskA = massage.massage(datafileA, 2)
--- src/massage.py
+++ src/massage.py
@@ -1,4 +1,10 @@
 import re
 
-def massage(taskA, taskBD, taskCE):
-    
+def massage(data, col):
+
+    tweets = []
+    for lines in data:
+        tweets.append(lines.split("\t"))
+        tempTweet = tweets[-1][col]
+        tempTweet = re.sub("(http|https):\/\/t\..*?\s", "httpstco", tempTweet)
+        #tempTweet = re.sub("([a-zA-Z])\1{2,}")
