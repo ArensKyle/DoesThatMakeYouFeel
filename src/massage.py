6e5ed229eee8fd1501dd737e23fb8e780d7ed85c
Kyle Arens <arenskyle@gmail.com>
2017-10-16 12:33:05 -0400

    additional commenting

--- src/massage.py
+++ src/massage.py
@@ -12,11 +12,14 @@ def massage(data, col):
     for lines in data:
         tknzr = TweetTokenizer()
 
+        #split record into tab separated values
         body = lines.split("\t")[-1]
         body = re.sub("(http|https):\/\/t\..*?\s", "httpstco", body)
         #body = re.sub("([a-zA-Z])\1{2,}", body)
         tokens = tknzr.tokenize(body)
 
+        #Conditional for the differnt formatting between task A data and data
+        #for other 4 tasks
         if (len(lines.split("\t")) == 3):
             sentiment = lines.split("\t")[1]
             twit_id = lines.split("\t")[0]
@@ -30,6 +33,7 @@ def massage(data, col):
         w_punc = []
         pos_tags = nltk.pos_tag(tokens)
 
+        #iterate through all tokens in the body of a given tweet
         for idx in range(len(tokens)):
             start_punc = -1
             pos = pos_tags[idx][1]
@@ -45,15 +49,19 @@ def massage(data, col):
             bagOfWords[key] = bagOfWords.get(key, 0) + 1
 
 
+        #turn bag of words into index of significant words for NN
         word_index = 0
         for word in bagOfWords:
             if(bagOfWords.get(word) > SIGNIFICANT):
                 sig_words[word] = word_index
                 word_index += 1
 
+        #calls the token parsing functionality in order to determine features
+        #vector for each word
         annotateTokens(w_tokens)
         tweet_obj = tweet.Tweet(w_tokens, twit_id, body, sentiment, subject)
 
+        #add the analyzed tweet into the final array for the neural net
         tweets.append(tweet_obj)
 
     return tweets, sig_words
--- src/tokenParse.py
+++ src/tokenParse.py
@@ -22,6 +22,7 @@ PUNCTUATION_MAPPING = {
   "?": QUESTION_F
 }
 
+#top level token parsing function. Calls more pecific functions
 def annotateTokens(tokens):
   lastPuncGroupIdx = 0
   for idx in range(len(tokens)):
@@ -29,23 +30,28 @@ def annotateTokens(tokens):
     annotateSpelling(tokens[idx])
     annotatePunctuation(idx, lastPuncGroupIdx, tokens)
 
+    #allows for the grouping of punctuation to account for repeating punctuation
     if (tokens[idx].word not in PUNCTUATION_SEPERATORS and idx > 0 and tokens[idx - 1].word in PUNCTUATION_SEPERATORS):
       # print("updating lastPuncGroup")
       lastPuncGroupIdx = idx
 
+#parses and accounts for punctuation features
 def annotatePunctuation(currentIdx, lastGroupIdx, tokens):
   if (tokens[currentIdx].word in PUNCTUATION_SEPERATORS):
     attr_idx = PUNCTUATION_MAPPING[tokens[currentIdx].word]
     for idx in range(lastGroupIdx, currentIdx):
       tokens[idx].attrs[attr_idx] += 1
 
+#weight hashtags
 def annotateHashtag(token):
   '''Take a token, and check to see if a token is a hashtag'''
   token.attrs[HASHTAG_F] = int(token.word[0] == "#" and len(token.word) > 1)
 
+#correct or incorrect spelling as a feature
 def annotateSpelling(token):
   '''Take a token, and check if is spelled correctly'''
   token.attrs[SPELL_F] = int(d.check(token.word))
+
 
 def testAnnotate():
   tokens = [words.Word("#test", 'VP'), words.Word(".",'VP'), words.Word("babe",'VP'), words.Word("!",'VP')]
