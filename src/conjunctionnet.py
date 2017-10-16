53c18899eae57873f8ebe9af85227aae0c0e7978
Kyle Arens <arenskyle@gmail.com>
2017-10-15 22:09:20 -0400

    refactored/reviewed joe's code

--- src/conjunctionnet.py
+++ src/conjunctionnet.py
@@ -2,19 +2,25 @@ import tensorflow as tf
 import conjunction
 
 def create_graph(categories):
-    cL = len(conjunction.CONJUNCTIONS)
+    c = len(conjunction.CONJUNCTIONS)
 
-    L = tf.placeholder(tf.float32, [None, categories])
-    R = tf.placeholder(tf.float32, [None, categories])
-    c = tf.placeholder(tf.float32, [None, cL])
-    
-    LW = tf.Variable(tf.zeros([cL, categories]))
-    RW = tf.Variable(tf.zeros([cL, categories]))
-    b = tf.Variable(tf.zeros([catagories]))
+    #left and right side category values (e.g. positive, negative)
+    left_side = tf.placeholder(tf.float32, [None, categories])
+    right_side = tf.placeholder(tf.float32, [None, categories])
+    #selected conjunction
+    conjuction = tf.placeholder(tf.float32, [None, c])
 
-    LWFilt = tf.matmul(c, LW)
-    RWFilt = tf.matmul(c, LW)
+    #left and right side weights for each conjunction
+    left_weights = tf.Variable(tf.zeros([c, categories]))
+    right_weights = tf.Variable(tf.zeros([c, categories]))
+    bias = tf.Variable(tf.zeros([categories]))
 
-    Sum = tf.multiply(L, LWFilt) + tf.multiply(R, RWFilt)
+    #scalars for left and right side wieghts based on the selected conjunction
+    left_scalar = tf.matmul(c, left_weights)
+    right_scalar = tf.matmul(c, right_weights)
 
-    return tf.nn.softmax(tf.nn.bias_add(Sum, b))
+    #combine the left and right sides
+    result = tf.multiply(left_side, left_scalar) + tf.multiply(right_side, right_scalar)
+
+    #apply bias when returning
+    return tf.nn.softmax(tf.nn.bias_add(result, bias))
