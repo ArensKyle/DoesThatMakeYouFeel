73f73746f6a9280ac6220412ee2dbf9316c03733
Kyle Arens <arenskyle@gmail.com>
2017-10-16 19:35:46 -0400

    added conjunction accounting to main

--- src/main.py
+++ src/main.py
@@ -15,7 +15,10 @@ def train():
     task_A = task_A_ret[0]
     task_A_word_index = task_A_ret[1]
 
-    
+    conj_returns = conj_classify_tweets(task_A)
+    task_A = conj_returns[0]
+    conj_A = conj_returns[1]
+
     #for tweet in task_A:
     #    for token in tweet.tokens:
     #        print(tweet.id, token.attrs, token.word)
@@ -26,6 +29,12 @@ def train():
     print("leaving word to vec")
     word_map_A = vector_returns_A[0]
     feat_map_A = vector_returns_A[1]
+
+    print("entering word to vec")
+    conj_vector_returns_A = wtv.sig_vec(conj_A, task_A_word_index)
+    print("leaving word to vec")
+    conj_word_map_A = vector_returns_A[0]
+    conj_feat_map_A = vector_returns_A[1]
 
 
 def test():
