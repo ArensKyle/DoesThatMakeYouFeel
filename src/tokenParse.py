cfb35b81104fe0b853e7b6bdc475f7949d57e6e3
Ryan Benner <rjbenner@hotmail.com>
2017-10-15 21:18:47 -0400

    added logic to annotate if a token is a hashtag

--- src/tokenParse.py
+++ src/tokenParse.py
@@ -1,4 +1,23 @@
+import words
+
+SPELL_F = words.new_feat()
+QUESTION_F = words.new_feat()
+EXCLAMATION_F = words.new_feat()
+HASHTAG_F = words.new_feat()
+PERIOD_F = words.new_feat()
+
 def annotateTokens(tokens):
-  for token in tokens
+  for token in tokens:
     #do stuff
-  return []
+    token=token
+  return tokens
+
+def annotateHashtag(token):
+  '''Take a token, and check to see if a token is a hashtag'''
+  token.attrs[HASHTAG_F] = int(token.word[0] == "#" and len(token.word) > 1)
+
+def testAnnotate():
+  token = words.Word("#test")
+  annotateHashtag(token)
+  print(HASHTAG_F)
+  print(token.attrs)
