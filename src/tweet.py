c97758fd02b4e1692e62eedce2fead7e955ea82c
Ryan Benner <rjbenner@hotmail.com>
2017-10-15 20:22:46 -0400

    add subject property

--- src/tweet.py
+++ src/tweet.py
@@ -1,9 +1,10 @@
 
 class Tweet:
-  def __init__(self, tokens=[], twitterId='', literal='', sentiment='', features={}):
+  def __init__(self, tokens=[], twitterId='', literal='', sentiment='', subject='', features={}):
     self.tokens = tokens
     self.id = twitterId
     self.literal = literal
     self.sentiment = sentiment
     self.attrs = features
+    self.subject = subject
 
