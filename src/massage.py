b443db01f33b5247f9ed9fada8c34fa3bf04936b
Kyle Arens <arenskyle@gmail.com>
2017-10-15 20:28:47 -0400

    tweets read in as objects now

--- src/massage.py
+++ src/massage.py
@@ -1,13 +1,27 @@
-import re
+import re, nltk,
 
 def massage(data, col):
 
     tweets = []
     for lines in data:
-        tweets.append(lines.split("\t"))
-        tempTweet = tweets[-1][col]
-        tempTweet = re.sub("(http|https):\/\/t\..*?\s", "httpstco", tempTweet)
-        #tempTweet = re.sub("([a-zA-Z])\1{2,}")
-        tweets[-1][col] = tempTweet
+        tknzr = TweetTokenizer()
+
+        body = lines.split("\t")[-1]
+        body = re.sub("(http|https):\/\/t\..*?\s", "httpstco", body)
+        #body = re.sub("([a-zA-Z])\1{2,}", body)
+        tokens = tknzr.tokenize(body)
+
+        if (len(lines.split("\t")) == 3):
+            sentiment = lines.split("\t")[1]
+            twitID = lines.split("\t")[0]
+            subject = ""
+        else:
+            sentiment = lines.split("\t")[2]
+            twitID = lines.split("\t")[0]
+            subject = lines.split("\t")[1]
+
+        tweetObj = tweet(tokens, twitID, body, sentiment, subject)
+
+        tweets.append(tweetObj)
 
     return tweets
--- src/tweet.py
+++ src/tweet.py
@@ -1,10 +1,8 @@
 
 class Tweet:
-  def __init__(self, tokens=[], twitterId='', literal='', sentiment='', subject='', features={}):
+  def __init__(self, tokens=[], twitterId='', literal='', sentiment='', subject=''):
     self.tokens = tokens
     self.id = twitterId
     self.literal = literal
     self.sentiment = sentiment
-    self.attrs = features
     self.subject = subject
-
