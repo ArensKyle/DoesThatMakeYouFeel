bf983b869ef4c9c58236b09258fac6471ca3ec2d
Kyle Arens <arenskyle@gmail.com>
2017-10-15 23:37:50 -0400

    Comments added

--- src/tweet.py
+++ src/tweet.py
@@ -1,8 +1,8 @@
 
 class Tweet:
   def __init__(self, tokens=[], twitterId='', literal='', sentiment='', subject=''):
-    self.tokens = tokens
-    self.id = twitterId
-    self.literal = literal
-    self.sentiment = sentiment
-    self.subject = subject
+    self.tokens = tokens #A list of all tokens present in the text body of the tweet
+    self.id = twitterId #the unique ID associated with a tweet
+    self.literal = literal #the text body of the tweet
+    self.sentiment = sentiment #the given sentiment for training data, or the discerned sentiment for testing data
+    self.subject = subject #the subject that is given with the tweet
--- src/words.py
+++ src/words.py
@@ -1,4 +1,5 @@
 
+#Will generate a uniue index for each feature used for analysis
 _FEAT_PTR = 0
 def new_feat():
     global _FEAT_PTR
@@ -6,10 +7,15 @@ def new_feat():
     _FEAT_PTR = _FEAT_PTR + 1
     return feature
 
+#returns the number of features that are being used
 def feat_len():
     global _FEAT_PTR
     return _FEAT_PTR
 
+#class to represent one word or token.
+#each word has a weighted list of relevant attributes
+#part of speech identifies words that are spelled same, but are different words
+# like the verb run, and the noun run
 class Word:
     def __init__(self, word, pos):
         self.word = word
