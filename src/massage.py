3546f140709b9f1a4a5ab1f7fec50ffa705a4186
Ryan Benner <rjbenner@hotmail.com>
2017-11-11 14:03:55 -0500

    add spell correcting and refactor massage.py and tokenparse.py

--- src/massage.py
+++ src/massage.py
@@ -1,5 +1,5 @@
 from nltk.tokenize import TweetTokenizer
-from tokenParse import annotateTokens
+from tokenParse import create_and_annotate_words
 import re, nltk, words, tweet
 
 SIGNIFICANT = 3
@@ -30,28 +30,15 @@ def massage(data, bag_of_words={}):
             twit_id = lines.split("\t")[0]
             subject = lines.split("\t")[1]
 
-        w_tokens = []
-        w_punc = []
-        pos_tags = nltk.pos_tag(tokens)
-
-        #iterate through all tokens in the body of a given tweet
-        for idx in range(len(tokens)):
-            start_punc = -1
-            pos = pos_tags[idx][1]
-
-            # create the Word
-            w_tokens.append(words.Word(
-                tokens[idx], # word
-                pos) # pos
-            )
-            if bag_of_words is not None:
-                # update bag of words
-                key = pos_tags[idx][0] + "_" + (pos)
+        # create feature vectors
+        w_tokens = create_and_annotate_words(tokens)
+        # print('printing w_tokens: ', w_tokens)
+        # update bag of words from tokens.
+        if bag_of_words is not None:
+            for token in w_tokens:
+                key = token.word + "_" + token.pos
                 bag_of_words[key] = bag_of_words.get(key, 0) + 1
 
-        #calls the token parsing functionality in order to determine features
-        #vector for each word
-        annotateTokens(w_tokens)
         tweet_obj = tweet.Tweet(w_tokens, twit_id, body, sentiment, subject)
 
         #add the analyzed tweet into the final array for the neural net
--- src/tokenParse.py
+++ src/tokenParse.py
@@ -1,6 +1,4 @@
-import words
-
-import enchant
+import enchant, nltk, words
 
 SPELL_F = words.new_feat()
 HASHTAG_F = words.new_feat()
@@ -21,6 +19,57 @@ PUNCTUATION_MAPPING = {
   "!": EXCLAMATION_F,
   "?": QUESTION_F
 }
+
+def create_and_annotate_words(tokens):
+  # We already have the tokens, now create the word type.
+  w_tokens = []
+
+  #iterate through all tokens in the body of a given tweet
+  for idx in range(len(tokens)):
+      # create the Word
+      token = words.Word(
+        tokens[idx], # word
+        '') # empty pos, will fill in later.
+
+      # do annotating that can be done on just a token.
+      annotateHashtag(token) # check if hashtag
+      annotate_and_correct_spelling(token) # update spelling
+      
+      w_tokens.append(words.Word(
+          tokens[idx], # word
+          '') # empty pos
+      )
+
+  # now that we have "corrected" spelling, we can properly pos tag the token list.
+  update_pos_tags(w_tokens)
+
+  # now annotate punctuation.
+  annotate_punctuations(w_tokens)
+  return w_tokens
+
+def get_words_from_token_list(token_list):
+  tokens = []
+  for token in token_list:
+    tokens.append(token.word)
+  return list(tokens)
+  
+def update_pos_tags(token_list):
+  # convert a list of tokens to just a list of words.
+  word_list = get_words_from_token_list(token_list)
+  pos_tags = nltk.pos_tag(word_list)
+  for idx in range(len(token_list)):
+    token_list[idx].pos = pos_tags[idx][1]
+
+def annotate_punctuations(token_list):
+  ''' takes in a list of tokens and annotates the punctuation vector '''
+  lastPuncGroupIdx = 0
+  for idx in range(len(token_list)):
+    annotatePunctuation(idx, lastPuncGroupIdx, token_list)
+
+    #allows for the grouping of punctuation to account for repeating punctuation
+    if (token_list[idx].word not in PUNCTUATION_SEPERATORS and idx > 0 and token_list[idx - 1].word in PUNCTUATION_SEPERATORS):
+      # print("updating lastPuncGroup")
+      lastPuncGroupIdx = idx
 
 #top level token parsing function. Calls more pecific functions
 def annotateTokens(tokens):
@@ -51,7 +100,25 @@ def annotateHashtag(token):
 def annotateSpelling(token):
   '''Take a token, and check if is spelled correctly'''
   if ("@" not in token.word):
-    token.attrs[SPELL_F] = int(spellDict.check(token.word))
+    spelledCorrectly = spellDict.check(token.word)
+    token.attrs[SPELL_F] = int(spelledCorrectly)
+
+  else:
+    token.attrs[SPELL_F] = 1
+
+def annotate_and_correct_spelling(token):
+  '''Takes a token, performs spell check then naively corrects spelling if mispelled'''
+  if (((token.word[0] != "#" and token.word[0] != "@") and len(token.word) > 1) or token.word != 'httpstco'):
+    # ignore #hashtags @mentions, 1 letter words, and our httpstco string.
+    spelledCorrectly = spellDict.check(token.word)
+    token.attrs[SPELL_F] = int(spelledCorrectly)
+    if (not spelledCorrectly):
+      # pyenchant offers a naive spellsuggestion where index zero is "most likely".
+      # print('token.word:', token.word, ' suggested:', spellDict.suggest(token.word))
+      suggestions = spellDict.suggest(token.word)
+      if (len(suggestions) > 0):
+        token.word = suggestions[0]
+
   else:
     token.attrs[SPELL_F] = 1
   
